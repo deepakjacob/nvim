70c4a90cb0bc1d2a26cd9f09f1a42deede8430e1
Deepak Jacob <deepak.jacob@gmail.com>
2024-03-30 19:03:15 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+require "jd.launch"
+require "jd.options"
+require "jd.keymaps"
+require "jd.autocmds"
+spec "jd.colorscheme"
+spec "jd.devicons"
+
+spec "jd.whichkey"
+spec "jd.telescope"
+spec "jd.telescope-tabs"
+spec "jd.lualine"
+spec "jd.nvimtree"
+
+spec "jd.schemastore"
+
+spec "jd.treesitter"
+spec "jd.mason"
+spec "jd.lspconfig"
+
+spec "jd.cmp"
+spec "jd.autopairs"
+spec "jd.comment"
+spec "jd.gitsigns"
+spec "jd.neogit"
+spec "jd.diffview"
+
+
+spec "jd.rust"
+spec "jd.neotab"
+spec "jd.fidget"
+
+require "jd.lazy"
---
+++ lua/jd/cmp.lua
@@ -0,0 +1,82 @@
+local M = {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-emoji",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-buffer",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-path",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-cmdline",
+      event = "InsertEnter",
+    },
+    {
+      "saadparwaiz1/cmp_luasnip",
+      event = "InsertEnter",
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    {
+      "hrsh7th/cmp-nvim-lua",
+    },
+    {
+      "roobert/tailwindcss-colorizer-cmp.nvim",
+    },
+  },
+  event = "InsertEnter",
+}
+
+function M.config()
+
+  local icons = require "jd.icons"
+  local types = require "cmp.types"
+  local cmp = require "cmp"
+  local luasnip = require "luasnip"
+
+  cmp.setup {
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    mapping = cmp.mapping.preset.insert ({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = {
+      { name = "luasnip" },
+      { name = "nvim_lsp" },
+      { name = "cmp_tabnine" },
+      { name = "nvim_lua" },
+      { name = "buffer" },
+      { name = "path" },
+      { name = "calc" },
+      { name = "emoji" },
+      { name = "treesitter" },
+      { name = "crates" },
+      { name = "tmux" },
+    },
+  }
+end
+
+return M
+
